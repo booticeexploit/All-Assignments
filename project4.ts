b374f893015e1c91f0a3471821492e72fa5806b6
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-03 03:40:36 -0700

    Add files via upload

---
+++ project4.ts
@@ -0,0 +1,4 @@
+const famousQuote: string = "A person who never made a mistake never tried anything new.";
+const author: string = "Albert Einstein";
+
+console.log(`${author} once said, "${famousQuote}"`);
