60cc5d0a52b2f363c6c4c1d90f83f83d13c5cd37
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-03 03:44:26 -0700

    Add files via upload

---
+++ Project5.js
@@ -0,0 +1,4 @@
+var famous_person = "Albert Einstein";
+var famousQuote = "A person who never made a mistake never tried anything new.";
+var message = "".concat(famous_person, " once said, \"").concat(famousQuote, "\"");
+console.log(message);
---
+++ Project5.ts
@@ -0,0 +1,5 @@
+const famous_person: string = "Albert Einstein";
+const famousQuote: string = "A person who never made a mistake never tried anything new.";
+
+const message: string = `${famous_person} once said, "${famousQuote}"`;
+console.log(message);
