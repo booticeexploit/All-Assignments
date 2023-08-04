cce33a1d39f1a358720bd54884d7b8bddb2d7ca4
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-04 09:22:03 -0700

    Add files via upload
    Assignment 13

---
+++ assignment13.js
@@ -0,0 +1,4 @@
+var bikes = ['a Motorbike', 'a Car', 'a Mountain Bike'];
+console.log("I would like to have ".concat(bikes[0]));
+console.log("I would like to have ".concat(bikes[1]));
+console.log("I would like to have ".concat(bikes[2]));
---
+++ assignment13.ts
@@ -0,0 +1,4 @@
+const bikes: string[] = ['a Motorbike' , 'a Car' , 'a Mountain Bike']
+console.log (`I would like to have ${bikes[0]}`)
+console.log (`I would like to have ${bikes[1]}`)
+console.log (`I would like to have ${bikes[2]}`)
