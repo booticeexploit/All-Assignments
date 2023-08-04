3351fe0493767f25dbc27106b342e1d79365125f
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-04 09:31:17 -0700

    Add files via upload

---
+++ Assignment14.ts
@@ -0,0 +1,4 @@
+const Guests: string[] = ["Mr. Ali" , "Mr Raza" , "Mr Bilal"]
+console.log (`${Guests[0]}, I would like to invite you to Dinner`)
+console.log (`${Guests[1]}, I would like to invite you to Dinner`)
+console.log (`${Guests[2]}, I would like to invite you to Dinner`)
