5db94f4fb7ac663aa8142f97e09419ad2bc75e61
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-04 09:15:47 -0700

    Add files via upload

---
+++ assignment12.js
@@ -0,0 +1,5 @@
+var names = ["Ali", "Ahmad", "Qasim", "Raza"];
+console.log("Hello, ".concat(names[0], "! How are you doing?"));
+console.log("Hello, ".concat(names[1], "! How are you doing?"));
+console.log("Hello, ".concat(names[2], "! How are you doing?"));
+console.log("Hello, ".concat(names[3], "! How are you doing?"));
---
+++ assignment12.ts
@@ -0,0 +1,6 @@
+const names: string[] = ["Ali" , "Ahmad", "Qasim", "Raza"]
+console.log(`Hello, ${names[0]}! How are you doing?`);
+console.log(`Hello, ${names[1]}! How are you doing?`);
+console.log(`Hello, ${names[2]}! How are you doing?`);
+console.log(`Hello, ${names[3]}! How are you doing?`);
+
