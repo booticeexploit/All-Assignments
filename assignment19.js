5abb867c305647a504032b00d359673266af48b8
Dawood Aftab <141323052+booticeexploit@users.noreply.github.com>
2023-08-09 05:50:32 -0700

    Add files via upload

---
+++ assignment19.js
@@ -0,0 +1,12 @@
+// Define an array of programming languages
+var programmingLanguages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "C#",
+    "Ruby",
+    "TypeScript"
+];
+// Print the array of programming languages
+console.log("Programming Languages:", programmingLanguages);
---
+++ assignment19.ts
@@ -0,0 +1,14 @@
+// Define an array of programming languages
+const programmingLanguages: string[] = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "C#",
+    "Ruby",
+    "TypeScript"
+  ];
+  
+  // Print the array of programming languages
+  console.log("Programming Languages:", programmingLanguages);
+  
